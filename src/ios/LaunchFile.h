284d2501f78aada8f7355f1d0f3be7aac3d9ba77
Tony Hursh <awh@acm.org>
2013-09-12 22:59:48 -0500

    Code formatting tweaks

--- src/ios/LaunchFile.h
+++ src/ios/LaunchFile.h
@@ -1,7 +1,7 @@
 #import <Cordova/CDV.h>
 
 @interface LaunchFile : CDVPlugin <UIDocumentInteractionControllerDelegate>
-@property(strong) UIDocumentInteractionController *doccontroller;
+@property (strong) UIDocumentInteractionController *doccontroller;
 - (void)launchFile:(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/LaunchFile.m
+++ src/ios/LaunchFile.m
@@ -5,18 +5,16 @@
 // Hat tip to Andrew Trice, http://www.tricedesigns.com/2012/08/15/open-with-in-ios-phonegap-apps/
 - (void)launchFile:(CDVInvokedUrlCommand*)command
 {
-    NSString* fileuri = [command.arguments objectAtIndex:0];
-  	NSString* escapeduri = [fileuri stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+	NSString* fileuri = [command.arguments objectAtIndex:0];
+	NSString* escapeduri = [fileuri stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
 	NSURL* fileURL = [NSURL URLWithString:escapeduri];	
 	_doccontroller = [UIDocumentInteractionController  interactionControllerWithURL:fileURL];
 	_doccontroller.delegate = self;
 	CDVViewController* cont = (CDVViewController*)[ super viewController ];
 	CGRect rect =  CGRectMake(0, 0, 1500.0f, 50.0f);
-    [_doccontroller presentOptionsMenuFromRect:rect inView:cont.view animated:YES];
-	 CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Opened."];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
+	[_doccontroller presentOptionsMenuFromRect:rect inView:cont.view animated:YES];
+	CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Opened."];
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
